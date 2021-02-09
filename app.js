dc23eca2724f5d7d3a181fd5f48c4562522ee5c7
qc <qchen125@gmail.com>
2021-02-09 18:47:43 -0500

    update with the branch stuff, doesn't work fully yet

--- app.js
+++ app.js
@@ -4,12 +4,37 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const favicon = require('serve-favicon');
+const User = require('./models/user');
+
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/gambitgames';
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+//'mongodb://localhost:27017/yelp-camp'
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+
 
 const app = express();
 
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', [path.join(__dirname, 'views'),
+    path.join(__dirname, 'views/users/'),
+    path.join(__dirname, 'views/partials/'),
+    path.join(__dirname, 'views/layouts/'),   
+        ]);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
@@ -24,17 +49,57 @@ app.use((req, res, next) => {
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('users/login')
+    }
+    next();
+}
+
+
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.get('/register', (req, res) => {
-    const { username = 'Annaymous' } = req.query;
-    req.session.username = username;
+//     const { username = 'Annaymous' } = req.query;
+//     req.session.username = username;
+    res.render('users/register')
+})
 
-});
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const user = new User({ username, password })
+    await user.save();
+    req.session.user_id = user._id;
+    res.redirect('/')
+})
 
-app.get('/gamestoday', (req, res) => {
+app.get('/login', (req, res) => {
+    res.render('users/login')
+})
+
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const foundUser = await User.findAndValidate(username, password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
+        res.redirect('/gameoutput');
+    }
+    else {
+        res.redirect('users/login')
+    }
+})
+
+app.post('/logout', (req, res) => {
+    req.session.user_id = null;
+    // req.session.destroy();
+    res.redirect('users/login');
+})
+
+
+app.get('/gamestoday', requireLogin, (req, res) => {
     res.render('gameoutput')
 })
 
